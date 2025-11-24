b8197cdb3899eb5de57feaa9b7f045d5ec6c2702
mickael <mickaelbruzzi0@gmail.com>
2025-11-24 10:00:42 +0100

    feature/useFetchData: app.tsx limpio

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,22 +1,9 @@
 import './App.css'
-import { useFetchData } from './hooks/useFetchData'
-
-interface childDetails {
-  id: number;
-  tutorId: number;
-  nombre: string;
-  apellido: string;
-  dob: string;
-
-}
 
 function App() {
-  const {data, isLoading, error} = useFetchData<childDetails[]>("/children");
   return (
     <>
       <h1>Hello World</h1> //pagina home
-
-      {data?.map(elem => <p>{elem.nombre}</p>)}
     </>
   )
 }
